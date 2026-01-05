7efe1abadbc019ee8a87af4417efd59ce05a332e
misterdhimz <dhimazanswer@gmail.com>
2026-01-05 07:23:22 +0700

    Konfigurasi hosting Vercel dan Supeabas1e

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,22 +9,17 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        if (config('app.env') === 'production') {
+   public function register(): void
+{
+    // Cek apakah sedang berjalan di Vercel (Production)
+    if (env('APP_ENV') === 'production') {
+        // Paksa folder storage ke /tmp agar bisa ditulis
+        $this->app->useStoragePath('/tmp/storage');
+
+        // Buat folder view secara paksa jika belum ada
         if (!is_dir('/tmp/storage/framework/views')) {
             mkdir('/tmp/storage/framework/views', 0755, true);
         }
     }
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        if (config('app.env') === 'production') {
-        config(['view.compiled' => '/tmp/storage/framework/views']);
-    }
-    }
+}
 }
